efc600f189efab1c58c5d41dae3b0a60361ced1b
memizzos <4321nitro1234@gmail.com>
2018-12-06 19:49:36 +0100

    zadanko

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Napisz funkcje która generuje losową liczbe (zwraca), jak argumenty przyjmuje
 // min (minimalna wartość) oraz max (największa możliwa do wygenerowania wartość)
+const buttonsKey = document.getElementsByClassName("key");
+let lastKey;
+
 
 function getRandomNumber(min, max){
     return Math.floor(Math.random() * (max-min+1) + min);
 }
+
+function getRandomKey() {
+   index = Math.floor(Math.random() * buttonsKey.length);
+    const key = buttonsKey[index];
+    if (key === lastKey){
+        console.log('The same key!!!!!!!!!!!!');
+        return getRandomKey();
+    }
+    lastKey = key;
+    return key;
+}
+
+console.log(getRandomKey());
+
+//Zad.dom
+// stwórz funkcje która losowo do klawisza dodaje klase fire-key
