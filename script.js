cd6cca7b4f0648caff693f123abe73ed43730363
memizzos <4321nitro1234@gmail.com>
2018-12-06 16:11:55 +0100

    wykonane zadanie domowe

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // Napisz funkcje która generuje losową liczbe (zwraca), jak argumenty przyjmuje
 // min (minimalna wartość) oraz max (największa możliwa do wygenerowania wartość)
+
+function losuj(min, max){
+    return Math.floor(Math.random() * (max-min+1) + min);
+}
