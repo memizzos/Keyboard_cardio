f0e4fdbd60d4c444bcfafc11b2be30f78db74eee
memizzos <4321nitro1234@gmail.com>
2018-12-13 19:51:12 +0100

    do następnych zajęć

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // min (minimalna wartość) oraz max (największa możliwa do wygenerowania wartość)
 const buttonsKey = document.getElementsByClassName("key");
 let lastKey;
+let currentKey;
+let OffGame = false;
+let score = 0;
 
 
 function getRandomNumber(min, max){
@@ -26,4 +29,32 @@ function peep(){
     const time = getRandomNumber(1000, 2000);
     const key = getRandomKey();
     key.classList.add('fire-key');
+    currentKey = key;
+    console.log(currentKey.attributes[0]);
+    setTimeout(() => {
+        key.classList.remove('fire-key');
+        if (!OffGame) {peep()};
+    }, time);
+}
+
+function endgame(){
+    OffGame = true;
+};
+function startGame(){
+window.addEventListener('keydown', checkKey);
+peep();
+OffGame = false;
+};
+
+function checkKey(event) {
+    console.log(currentKey);
+    if (event.keyCode == currentKey.attributes[0].value){
+        score++;
+        console.log(score);
+
+    }
+    else{
+        score--;
+        console.log(score);
+    }
 }
