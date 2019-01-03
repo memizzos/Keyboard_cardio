6df044cdb4cad9044f2b0cce8b5dac8b732448a6
Andrzej <deceeededced>
2019-01-03 19:23:41 +0100

    update-cik

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let lastKey;
 let currentKey;
 let OffGame = false;
 let score = 0;
+let endScore = 20;
 
 
 function getRandomNumber(min, max){
@@ -32,29 +33,56 @@ function peep(){
     currentKey = key;
     console.log(currentKey.attributes[0]);
     setTimeout(() => {
-        key.classList.remove('fire-key');
         if (!OffGame) {peep()};
     }, time);
 }
+function addClassToElement(className, element){
+    element.classList.add(className);
+    const time = getRandomNumber(1000, 2000);
+    setTimeout(() => {
+        removeClassFromElement(className, element)
+    }, time);
+}
+
+
+function removeClassFromElement(className, element){
+    time = getRandomNumber(1000, 2000);
+    setTimeout(element.classList.remove(className), time);
+}
+
+
+function showScore(){
+    menu = document.querySelector(".game-menu");
+    menu.innerHTML = `<div class="scoreBoard">Score: ${score}</div>`;
+};
 
 function endgame(){
     OffGame = true;
 };
+
 function startGame(){
-window.addEventListener('keydown', checkKey);
-peep();
-OffGame = false;
+    showScore()
+    window.addEventListener('keydown', checkKey);
+    peep();
+    OffGame = false;
+    scoreBoard = document.querySelector('.scoreBoard')
 };
 
 function checkKey(event) {
-    console.log(currentKey);
     if (event.keyCode == currentKey.attributes[0].value){
-        score++;
+        score ++;
+        removeClassFromElement('fire-key', currentKey);
+        addClassToElement('correct-key-down', currentKey);
         console.log(score);
-
     }
     else{
-        score--;
+        score --;
+        let wrongKey = document.querySelector(`div[data-key="${event.keyCode}"]`);
         console.log(score);
+        addClassToElement('wrong-key-down', wrongKey);
+    }
+    scoreBoard.innerHTML = `Score: ${score}`;
+    if (score == endScore) {
+        endgame()
     }
 }
